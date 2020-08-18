8077924d1c5059cb42dfd01a85d5efcb9a50d11a
Stephen Wan <stephen@stephenwan.com>
2020-08-17 20:07:40 -0700

    internal/printer: add benchmark for printing
    Current results:
    $ go test -bench=. internal/printer/*.go
    goos: darwin
    goarch: amd64
    BenchmarkPrinter-12    	    2677	    385821 ns/op	  381227 B/op	      19 allocs/op

--- internal/printer/bench_test.go
+++ internal/printer/bench_test.go
@@ -1,14 +1,15 @@
-package parser
+package printer
 
 import (
 	"io/ioutil"
 	"testing"
 
 	"github.com/stephen/cssc/internal/lexer"
+	"github.com/stephen/cssc/internal/parser"
 	"github.com/stretchr/testify/require"
 )
 
-func BenchmarkParser(b *testing.B) {
+func BenchmarkPrinter(b *testing.B) {
 	b.ReportAllocs()
 
 	by, err := ioutil.ReadFile("../testdata/bootstrap.css")
@@ -17,9 +18,10 @@ func BenchmarkParser(b *testing.B) {
 		Path:    "bootstrap.css",
 		Content: string(by),
 	}
+	ast := parser.Parse(source)
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		Parse(source)
+		_ = Print(ast)
 	}
 }
