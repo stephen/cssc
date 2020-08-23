b752df8c0cde58922d1bac1a9c802b357a6e46f0
Stephen Wan <stephen@stephenwan.com>
2020-08-23 14:14:55 -0700

    internal/printer: save allocation, speed up lookup
    We save an allocation by reusing the byte in the lookup array. We have to use
    a byte array instead because otherwise go will still do an allocation
    to turn string into byte slice.
    
    We also reify the reverse lookup so that decoding does not require
    a byte search. This one seems less interesting (50% speedup to 4ns
    and no allocation change).
    
    Before:
    ```
    BenchmarkVLQEncode/short_encode-12         	53238228	        22.8 ns/op	       8 B/op	       1 allocs/op
    BenchmarkVLQEncode/long_encode-12          	38394794	        29.7 ns/op	       8 B/op	       1 allocs/op
    BenchmarkVLQEncode/short_decode-12         	172432921	         6.96 ns/op	       0 B/op	       0 allocs/op
    BenchmarkVLQEncode/long_decode-12          	158936592	         7.42 ns/op	       0 B/op	       0 allocs/op
    ```
    
    After:
    ```
    BenchmarkVLQEncode/short_encode-12         	325493589	         3.62 ns/op	       0 B/op	       0 allocs/op
    BenchmarkVLQEncode/long_encode-12          	36126438	        31.8 ns/op	       8 B/op	       1 allocs/op
    BenchmarkVLQEncode/short_decode-12         	316624790	         3.77 ns/op	       0 B/op	       0 allocs/op
    BenchmarkVLQEncode/long_decode-12          	269060397	         4.81 ns/op	       0 B/op	       0 allocs/op
    ```

--- internal/printer/sourcemaps.go
+++ internal/printer/sourcemaps.go
@@ -1,11 +1,74 @@
 package printer
 
-import (
-	"strings"
-)
+var (
+	base64 = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/")
 
-const (
-	base64 = string("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/")
+	base64Reverse = [127]byte{
+		65:  0,
+		66:  1,
+		67:  2,
+		68:  3,
+		69:  4,
+		70:  5,
+		71:  6,
+		72:  7,
+		73:  8,
+		74:  9,
+		75:  10,
+		76:  11,
+		77:  12,
+		78:  13,
+		79:  14,
+		80:  15,
+		81:  16,
+		82:  17,
+		83:  18,
+		84:  19,
+		85:  20,
+		86:  21,
+		87:  22,
+		88:  23,
+		89:  24,
+		90:  25,
+		97:  26,
+		98:  27,
+		99:  28,
+		100: 29,
+		101: 30,
+		102: 31,
+		103: 32,
+		104: 33,
+		105: 34,
+		106: 35,
+		107: 36,
+		108: 37,
+		109: 38,
+		110: 39,
+		111: 40,
+		112: 41,
+		113: 42,
+		114: 43,
+		115: 44,
+		116: 45,
+		117: 46,
+		118: 47,
+		119: 48,
+		120: 49,
+		121: 50,
+		122: 51,
+		48:  52,
+		49:  53,
+		50:  54,
+		51:  55,
+		52:  56,
+		53:  57,
+		54:  58,
+		55:  59,
+		56:  60,
+		57:  61,
+		43:  62,
+		47:  63,
+	}
 )
 
 // VLQEncode encodes a signed int into base64 byte slice.
@@ -23,7 +86,11 @@ func VLQEncode(in int32) []byte {
 		in <<= 1
 	}
 
-	// XXX: allocation?
+	if in>>5 == 0 {
+		offset := in & 0b11111
+		return []byte(base64[offset : offset+1])
+	}
+
 	var rv []byte
 	for {
 		// Content bits.
@@ -60,7 +127,7 @@ func VLQDecode(in []byte) (value int32, len int32) {
 	for _, b := range in {
 		read++
 
-		cur := int32(strings.IndexByte(base64, b))
+		cur := int32(base64Reverse[b])
 
 		rv += (cur & 0b11111) << shift
 
